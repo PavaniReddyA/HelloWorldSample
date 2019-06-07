b0ce6be43d4fa26e600c5006d295ef551da0b9f0
pavani.allappareddy <pavani.allappareddy@EXAIN-IT-L0049.exa-ag.local>
2019-06-07 12:05:38 +0530

    Changes

--- HelloWorldSample/src/com/sample/helloworld/Sample1.java
+++ HelloWorldSample/src/com/sample/helloworld/Sample1.java
@@ -1,9 +1,11 @@
 package com.sample.helloworld;
 
-public class HelloWorld {
+public class Sample1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");	
+		
+		
+
 	}
 
 }
